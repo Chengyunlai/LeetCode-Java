583e59193d0eb1f135641e980eabc6862b96ac5a
chengyunlai <1257997441@qq.com>
2022-11-15 16:40:41 +0800

    二分查找

---
+++ src/BinarySearch/Answer.java
@@ -0,0 +1,38 @@
+package BinarySearch;
+
+/**
+ * @ClassName
+ * @Description
+ * @Author:chengyunlai
+ * @Date
+ * @Version 1.0
+ **/
+public class Answer {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int search = solution.search(new int[]{1}, 1);
+        // 输出下标
+        System.out.println(search);
+    }
+}
+
+class Solution {
+    public int search(int[] nums, int target) {
+        // 首指针
+        int left = 0;
+        // 尾指针
+        int right = nums.length-1;
+        while (left <= right){
+            // 中间值
+            int mid = left + (right - left) / 2;
+            if (target == nums[mid]){
+                return mid;
+            }else if(target < nums[mid]){
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+        return -1;
+    }
+}
